efa3d7636dfeefc2a975fe1ac2eef8bda218e0f5
gustavo5855 <gustavo.soares.cdc@gmail.com>
2017-09-04 01:12:05 -0300

    so

--- trabalho_1/window.py
+++ trabalho_1/window.py
@@ -4,6 +4,7 @@ def create_gram(n):
     with open("syscalls/log_syscall_cp_without_id.txt", "r") as file:
         with open("teste_novo.txt", "w") as arq:
             data = [x[:-1] for x in file]
+            # print (data)
             for i in range(len(data) - n + 1):
                 teste = arq.write(str(data[i:i+n])[1:-1].replace("\'","") + "\n")
 
