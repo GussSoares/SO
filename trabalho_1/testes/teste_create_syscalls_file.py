d879563979d918336de29850f9e986ac0fc46bb0
gustavo5855 <gustavo.soares.cdc@gmail.com>
2017-09-04 14:07:38 -0300

    so

---
+++ trabalho_1/testes/teste_create_syscalls_file.py
@@ -0,0 +1,7 @@
+with open("syscalls/log_mv_without_id.txt") as file:
+    with open("log_syscall_mv_with_id.txt", "w") as arq:
+        for line in file:
+            result = line.split("(")[0]
+            if result[1] != "+":
+                # arq.write(result+"\n")
+                print(result)
