9d4564726bbea26b7d461453cb815bdc43d9c05b
gustavo5855 <gustavo.soares.cdc@gmail.com>
2017-09-05 14:32:27 -0300

    so

--- trabalho_1/testes/teste_create_syscalls_file.py
+++ trabalho_1/testes/teste_create_syscalls_file.py
@@ -1,6 +1,6 @@
 
 #This function creates a log file that contains the id and system calls, of the commands cp and mv
-def _create_log_file(file="log_cp.txt",arq="teste_cp.txt"):
+def _create_log_file(file="log_mv.txt",arq="teste_mv.txt"):
     with open(file,"r") as file1:
         with open(arq,"w") as arq1:
             for line in file1:
@@ -9,10 +9,10 @@ def _create_log_file(file="log_cp.txt",arq="teste_cp.txt"):
                     arq1.write(result+"\n")
                     print(result)
 
-#_create_log_file(file="log_cp.txt", arq="teste_cp.txt")
+# _create_log_file(file="log_mv.txt", arq="teste_mv.txt")
 
 #This function creates a log file that contains the system calls, of the commands cp and mv
-def _create_log_without_id(file="teste_cp.txt",arq="teste_cp_without_id.txt"):
+def _create_log_without_id(file="teste_mv.txt",arq="teste_mv_without_id.txt"):
     with open(file) as file1, open(arq, "w") as arq1:
         for line in file1:
             result = line.split("] ")[1]
@@ -20,16 +20,16 @@ def _create_log_without_id(file="teste_cp.txt",arq="teste_cp_without_id.txt"):
                 arq1.write(result)
                 print(result)
 
-#_create_log_without_id(file="teste_cp.txt", arq="teste_cp_without_id.txt")
+# _create_log_without_id(file="teste_mv.txt", arq="teste_mv_without_id.txt")
 
 #This function creates a n-grams file of the syscalls of the commands cp and mv
-def _create_gram(n, file="log_syscall_mv_without_id.txt", arq="n_gram_mv.txt"):
+def _create_gram(n, file="teste_cp_without_id.txt", arq="n_gram_cp.txt"):
     with open(file, "r") as file1, open(arq, "w") as arq1:
         data = [x[:-1] for x in file1]
         for i in range(len(data) - n + 1):
             arq1.write(str(data[i:i+n])[1:-1].replace("\'","") + "\n")
 
-#_create_gram(10, file="log_syscall_mv_without_id.txt", arq="n_gram_mv.txt")
+# _create_gram(10, file="teste_cp_without_id.txt", arq="n_gram_cp.txt")
 
 #This function does a comparation between two files, healthy and infected
 def _compare(healthy="n_gram_cp.txt", infected="n_gram_mv.txt"):
@@ -37,11 +37,11 @@ def _compare(healthy="n_gram_cp.txt", infected="n_gram_mv.txt"):
 
         arq1 = [z[:-1] for z in arq_healthy]
         arq2 = [y[:-1] for y in arq_infected]
-        inter = set(arq1).intersection(arq2)
-        list = [x[:-1] for x in inter]
+        inter = set(arq2).intersection(arq1)
 
-        print("Elements file1: "+str(arq1)+"\n")
-        print("Elements file2: "+str(arq2)+"\n")
-        print("Infection: "+str(100-(len(list) / len(arq2))*100) + "%")
+        # print(len(arq2)-len(list))
+        # print("Elements file1: "+str(arq1)+"\n")
+        # print("Elements file2: "+str(arq2)+"\n")
+        print("Infection: ", (len(arq2)-len(inter))/len(arq2)*100,"%")
 
 _compare(healthy="n_gram_cp.txt", infected="n_gram_mv.txt")
