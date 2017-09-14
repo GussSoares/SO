8db17e5a7cec052ab02238b1242c1d72f9af100c
gustavo5855 <gustavo.soares.cdc@gmail.com>
2017-09-13 22:07:02 -0300

    so

--- trabalho_2/item _b.py
+++ trabalho_2/item _b.py
@@ -1,1 +1,9 @@
-print ("text")
+def _create_gram(n, file="teste_cp_without_id.txt", arq="n_gram_cp.txt"):
+    with open(file, "r") as file1, open(arq, "w") as arq1:
+        data = [x[:-1] for x in file1]
+        for i in range(len(data) - n + 1):
+            arq1.write(str(data[i:i+n])[1:-1].replace("\'","") + "\n")
+            print (str(data[i:i+n])[1:-1].replace("\'","") + "\n")
+
+for n in range (1,11,1):
+    _create_gram(n, file="teste_cp_without_id.txt", arq="n_gram_cp.txt")
